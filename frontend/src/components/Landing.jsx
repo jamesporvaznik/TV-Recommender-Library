44c1d213d1fc04110f9ba32f42d0244be414b24f
jamesporvaznik <jhporvaznik@gmail.com>
2025-11-20 13:17:49 -0500

    Update Landing.jsx

--- frontend/src/components/Landing.jsx
+++ frontend/src/components/Landing.jsx
@@ -11,7 +11,7 @@ const Landing = ({shows, watchedIds, bookmarkedIds, onToggleList, onCardClick}) 
         const meetsRating = show.rating_avg >= 7;
         const meetsReviewCount = show.vote_count > 500;
         
-        return meetsRating && meetsReviewCount;
+        return meetsRating && meetsReviewCount
     });
   
   return (
@@ -21,16 +21,24 @@ const Landing = ({shows, watchedIds, bookmarkedIds, onToggleList, onCardClick}) 
       <div className="mt-10 max-w-5xl mx-auto">
         
         <div className="flex flex-col sm:flex-row items-center mb-8">
-          <h2 className="text-xl font-semibold text-gray-500 mx-auto">Discover shows you love</h2>
-          <div className="mt-2 sm:mt-0 sm:ml-4">
-            {/* Refresh button */}
-            <button
-              className="px-3 py-1 text-sm font-semibold bg-gray-100 rounded"
-              onClick={() => setRefreshKey(k => k + 1)}
-            >
-              Refresh
-            </button>
-          </div>
+    
+            {/* 1. Placeholder Div (Balances the Button's space) */}
+            <div className="hidden sm:block sm:w-20"></div> 
+            
+            {/* 2. The Title (Centered) */}
+            <h2 className="text-xl font-semibold text-gray-400 mx-auto">
+                Discover shows you love
+            </h2>
+            
+            {/* 3. The Button Wrapper */}
+            <div className="mt-2 sm:mt-0 sm:ml-4 sm:w-20 text-right mr-4"> 
+                <button
+                    className="px-5 py-1 text-sm border rounded-xl bg-neutral-900 font-semibold shadow-sm hover:bg-zinc-800 border-gray-300"
+                    onClick={() => setRefreshKey(k => k + 1)}
+                >
+                    Refresh
+                </button>
+            </div>
         </div>
 
         {/* Displaying some random shows for the users to see */}
@@ -62,11 +70,11 @@ const Landing = ({shows, watchedIds, bookmarkedIds, onToggleList, onCardClick}) 
         {/* left col */}
         <div className="md:w-1/2">
           <div className="font-bold text-3xl md:text-6xl max-w-lg text-left">
-            <p className="text-gray-700">
+            <p className="text-white">
               Track and Rate Your Watchlist
             </p>
           </div>
-          <div className="mt-5 font-semibold text-gray-600 max-w-md text-left">
+          <div className="mt-5 font-semibold text-gray-400 max-w-md text-left">
             <p>This feature will allow you to keep track of all the shows you have watched and rate them so you remember and can find similar shows to your favorites.</p>
           </div>
         </div>
@@ -97,11 +105,11 @@ const Landing = ({shows, watchedIds, bookmarkedIds, onToggleList, onCardClick}) 
 
         <div className="md:w-1/2 flex flex-col items-center md:items-end md:text-right">
           <div className="font-bold text-3xl md:text-6xl max-w-lg">
-            <p className="text-gray-700">
+            <p className="text-white">
                 Bookmark Shows to Watch Later
             </p>
           </div>
-          <div className="mt-5 font-semibold text-gray-600 max-w-md">
+          <div className="mt-5 font-semibold text-gray-400 max-w-md">
             <p>This feature will allow you to keep track of the shows you want to watch next. Users will also be able to access information on the show like where they can watch it and key details</p>
           </div>
         </div>
@@ -111,11 +119,11 @@ const Landing = ({shows, watchedIds, bookmarkedIds, onToggleList, onCardClick}) 
       <div className="mt-72 max-w-5xl mx-auto px-4 md:px-0 flex flex-col md:flex-row md:items-start md:justify-between gap-8">
         <div className="md:w-1/2">
           <div className="font-bold text-3xl md:text-6xl max-w-lg text-left">
-            <p className="text-gray-700">
+            <p className="text-white">
               Search and Discover new Shows
             </p>
           </div>
-          <div className="mt-5 font-semibold text-gray-600 max-w-md text-left">
+          <div className="mt-5 font-semibold text-gray-400 max-w-md text-left">
             <p>In this search feature you can filter by many key features to find new shows or search shows by name to find familiar shows</p>
           </div>
         </div>
@@ -145,11 +153,11 @@ const Landing = ({shows, watchedIds, bookmarkedIds, onToggleList, onCardClick}) 
 
         <div className="md:w-1/2 flex flex-col items-center md:items-end md:text-right">
           <div className="font-bold text-3xl md:text-6xl max-w-lg">
-            <p className="text-gray-700">
+            <p className="text-white">
                 Find New Shows with Custom Recommendations
             </p>
           </div>
-          <div className="mt-5 font-semibold text-gray-600 max-w-md">
+          <div className="mt-5 font-semibold text-gray-400 max-w-md">
             <p>This recommendation engine takes into account key details from your favorite shows to find new shows with similarites </p>
           </div>
         </div>
