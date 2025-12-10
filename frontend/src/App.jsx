b08b9a2d5ec28bb11d65dd104ae81ac03926fa06
jamesporvaznik <jhporvaznik@gmail.com>
2025-12-10 16:34:09 -0500

    changing configs to host publicly
    Was testing locally, so needed to change some configs to host publicly.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -54,8 +54,8 @@ function App() {
     const [isSearhQuery, setIsSearchQuery] = useState(false);
     const [isWatched, setIsWatched] = useState(false);
     const isMobile = useScreenSize(768);
-    // const API_BASE_URL = import.meta.env.VITE_API_URL || '';
-    const API_BASE_URL = 'http://localhost:5173';
+    const API_BASE_URL = import.meta.env.VITE_API_URL || '';
+    // const API_BASE_URL = 'http://localhost:5173';
     
     //Function called when you click on a mode in the sidebar
     const changeMode = async (newMode) => { 
